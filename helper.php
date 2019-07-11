79bad1df5edbb62fb41a386726af003e45cf2ec2
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-07-11 16:34:14 +0100

    v1.0.0

---
+++ helper.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_toc
+ *
+ * @copyright   Copyright (C) NPEU 2019.
+ * @license     MIT License; see LICENSE.md
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\String\StringHelper;
+
+/**
+ * Helper for mod_toc
+ */
+class ModTocHelper
+{
+    /**
+     * Does a thing
+     *
+     * @param   \Joomla\Registry\Registry  &$params  module parameters object
+     *
+     * @return  mixed
+     */
+    /*public static function getSomething(&$params)
+    {
+        
+    }*/
+}
